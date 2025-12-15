85892ed7365c4bdef082e7221179f6d163bcf947
Jeremy Arrascaeta <jezza5152@gmail.com>
2025-12-15 22:51:06 +0000

    Linting and checking validity of types ... Failed to compile. ./src/comp

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -57,11 +57,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Dropdown: ({ value, onChange, children, ...props }: DropdownProps) => {
-          const options = React.Children.toArray(
-            children
-          ) as React.ReactElement<React.HTMLProps<HTMLOptionElement>>[]
-          const selected = options.find((child) => child.props.value === value)
+        Dropdown: ({ value, onChange, options, ...props }: DropdownProps) => {
+          const selected = options?.find((option) => option.value === value);
           const handleChange = (value: string) => {
             const changeEvent = {
               target: { value },
@@ -76,16 +73,17 @@ function Calendar({
               }}
             >
               <SelectTrigger className="pr-1.5 focus:ring-0">
-                <SelectValue>{selected?.props?.children}</SelectValue>
+                <SelectValue>{selected?.label}</SelectValue>
               </SelectTrigger>
               <SelectContent position="popper">
                 <ScrollArea className="h-80">
-                  {options.map((option, id: number) => (
+                  {options?.map((option, id: number) => (
                     <SelectItem
-                      key={`${option.props.value}-${id}`}
-                      value={option.props.value?.toString() ?? ""}
+                      key={`${option.value}-${id}`}
+                      value={option.value.toString() ?? ""}
+                      disabled={option.disabled}
                     >
-                      {option.props.children}
+                      {option.label}
                     </SelectItem>
                   ))}
                 </ScrollArea>
